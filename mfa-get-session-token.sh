83dd2ff802234e73030f4bf69de24d6ec29279e6
hez <christianheselton@gmail.com>
2022-08-14 09:51:25 +0100

    cleaned up typos

--- mfa-get-session-token.sh
+++ mfa-get-session-token.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-## quick script to assume roles and pass details to aws config
+## quick script to grab a session token for an MFA arn and update AWS Creds file
 ## get role information
-read -p 'Enter MFA ARN: ' mfa_arn
+read -p 'Enter ARN of MFA account: ' mfa_arn
 read -p 'Enter MFA Token: ' token
 read -p 'Enter Profile to use: ' profile_name
 
-# call role assumption
+# call STS for session token
 temp_role=$(aws sts get-session-token --serial-number $mfa_arn --token-code $token --profile $profile_name)
 
-
-#temp_role=$(aws sts assume-role --role-arn $role_arn --role-session-name $role_session_name --profile $profile_name)
-
-# update aws configuration
 # export variables
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
 
-# create new profile in config
+# Add session to AWS config
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile mfa
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile mfa
 aws configure set aws_session_token $AWS_SESSION_TOKEN --profile mfa
